67da276e67d7da07f5f6dbe1305ffcc24acae22e
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2014-12-31 16:53:21 +0000

    WIP: Makefile doesn't work

---
+++ src/abnf_rfc2234/abnf_grammar_main.ml
@@ -0,0 +1,17 @@
+open Abnf_grammar_p4
+
+let main () = 
+  let fname = Sys.argv.(1) in
+  let Some txt = read_file_as_string fname in
+  let eg = run_parser_string parse_S txt in
+  ()
+
+let _ = main ()
+
+
+(*
+Local Variables:
+mode: tuareg
+End:
+*)
+
---
+++ src/abnf_rfc2234/p4_abnf_codegen.ml
@@ -0,0 +1,3 @@
+(* code generation for abnf *)
+
+open Abnf_grammar_p4
