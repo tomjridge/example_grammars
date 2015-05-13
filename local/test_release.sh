f1e954a04c4286957112b92c169bd25021f1315f
Tom Ridge <tom.j.ridge@googlemail.com>
2015-05-13 12:25:04 +0100

    +

--- local/test_release.sh
+++ local/test_release.sh
@@ -2,4 +2,20 @@ git clone -b dev https://github.com/tomjridge/e3.git
 git clone -b dev https://github.com/tomjridge/p1.git
 git clone -b dev https://github.com/tomjridge/p4.git
 git clone https://github.com/tomjridge/example_grammars.git
-cd example_grammars && nix-build
+
+# to build via nix
+function build_with_nix {
+  cd example_grammars && nix-build
+}
+
+
+# to build locally
+function build_locally {
+  cd example_grammars && ln -sf local/Makefile.local . && cd ..
+  cd p4 && ln -sf ../example_grammars/Makefile.local . && cd ..
+  cd example_grammars && local/make.sh && cd ..
+  cd example_grammars && make
+}
+
+build_with_nix
+
