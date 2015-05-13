37bb346f01538a9a9d09992d0167383f04edc770
Tom Ridge <tom.j.ridge@googlemail.com>
2015-05-13 12:09:20 +0100

    +

---
+++ local/test_release.sh
@@ -0,0 +1,5 @@
+git clone -b dev https://github.com/tomjridge/e3.git
+git clone -b dev https://github.com/tomjridge/p1.git
+git clone -b dev https://github.com/tomjridge/p4.git
+git clone https://github.com/tomjridge/example_grammars.git
+cd example_grammars && nix-build
